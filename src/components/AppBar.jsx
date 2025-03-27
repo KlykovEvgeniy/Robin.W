d8e61a9f733e3e8c02fe1ab93f33e34a66bd3f29
Jeny Klikov <klikovevgeniy2000@mail.ru>
2025-03-27 16:57:12 +0300

    project end

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,74 @@
-export default function AppBar({setVis}) {
+import { useEffect, useState } from 'react';
+import { useWindowWidth } from '@react-hook/window-size';
+import { hamburger } from '../images';
+import { IoClose } from 'react-icons/io5';
+
+export default function AppBar() {
+  const [vis, setVis] = useState(false);
+  const width = useWindowWidth();
+
+  useEffect(() => {
+    if (width <= 640) {
+      setVis(false);
+    } else {
+      setVis(true);
+    }
+  }, [width]);
+
+  const toggleVis = () => {
+    setVis((prev) => !prev);
+    document.body.classList.toggle('overflow-y-hidden');
+  };
 
   const handleClick = () => {
-    document.body.classList.remove('overflow-y-hidden')
-    setVis(false)
-  }
+    if (width > 640) {
+      return;
+    }
+    document.body.classList.remove('overflow-y-hidden');
+    setVis(false);
+  };
 
   return (
-    <nav className="h-full sm:z-20 sm:flex sm:absolute sm:top-0 sm:right-0 sm:w-full sm:items-center sm:h-[110vh] sm:justify-center sm:bg-black">
-      <ul className="flex space-x-16 items-center sm:flex-col sm:space-x-0 sm:items-center xl:space-x-5">
-        <li>
-          <a onClick={handleClick} className="link-nav" href="#exp">Experience</a>
-        </li>
-        <li>
-          <a onClick={handleClick} className="link-nav" href="#work">Work</a>
-        </li>
-        <li>
-          <a onClick={handleClick} className="link-nav" href="#photo">Photography</a>
-        </li>
-        <li>
-          <a onClick={handleClick} href="#contact" className="link-nav bg-[#5221E6] h-full p-[38px] -ml-10 hover:bg-transparent hover:text-white sm:p-0 sm:bg-transparent sm:-m-0 xl:-m-3">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {vis && (
+        <nav className="h-full sm:z-20 sm:flex sm:absolute sm:top-0 sm:right-0 sm:w-full sm:items-center sm:h-[110vh] sm:justify-center sm:bg-black">
+          <ul className="flex space-x-16 items-center sm:flex-col sm:space-x-0 sm:items-center xl:space-x-5">
+            <li>
+              <a onClick={handleClick} className="link-nav" href="#exp">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a onClick={handleClick} className="link-nav" href="#work">
+                Work
+              </a>
+            </li>
+            <li>
+              <a onClick={handleClick} className="link-nav" href="#photo">
+                Photography
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={handleClick}
+                href="#contact"
+                className="link-nav bg-[#5221E6] h-full p-[38px] -ml-10 hover:bg-transparent hover:text-white sm:p-0 sm:bg-transparent sm:-m-0 xl:-m-3"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
+      {width <= 640 && (
+        <button className="z-50" onClick={toggleVis}>
+          {vis ? (
+            <IoClose className="fill-[#C4C4C4] w-6 h-6" />
+          ) : (
+            <img className="w-6 h-6" src={hamburger} alt="hamburger button" />
+          )}
+        </button>
+      )}
+    </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,13 @@
-import { useEffect, useState } from 'react';
 import AppBar from './AppBar';
 import Logo from './Logo';
 import Hero from './Hero';
-import { hamburger } from '../images';
-import { useWindowWidth } from '@react-hook/window-size';
-import { IoClose } from 'react-icons/io5';
 
 export default function Header() {
-  const [vis, setVis] = useState(false);
-  const width = useWindowWidth();
-
-  useEffect(() => {
-    if (width <= 640) {
-      setVis(false);
-    } else {
-      setVis(true);
-    }
-  }, [width]);
-
-  const toggleVis = () => {
-    setVis((prev) => !prev);
-    document.body.classList.toggle('overflow-y-hidden')
-  };
-
   return (
     <header className="bg w-full p-5 mb-30 h-full pb-20 lg:pb-5 md:pb-1 sm:pb-0">
       <div className="container flex justify-between items-center mb-14 md:mb-5">
         <Logo />
-        {vis && <AppBar vis={setVis}/>}
-        {width <= 640 && (
-          <button className="z-50" onClick={toggleVis}>
-            {vis ? <IoClose className="fill-[#C4C4C4] w-6 h-6" /> : <img className='w-6 h-6' src={hamburger} alt="hamburger button" />}
-          </button>
-        )}
+        <AppBar />
       </div>
       <div className='container'>
         <Hero />
